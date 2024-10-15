06151995ca0ce92831c4fd890124acf71973e183
MatthiasChometon <matthias.chometon@emage-me.com>
2024-10-15 20:46:36 +0200

    build: fix install production build packages

--- infrastructure/translation/nuxt.config.ts
+++ infrastructure/translation/nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
 	modules: ['@nuxtjs/i18n'],
 	i18n: {
-		vueI18n: './i18n.config.ts',
+		vueI18n: '@/infrastructure/translation/i18n.config',
 		lazy: true
 	}
 })
