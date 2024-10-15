dd1a0bff791b677ca359aab88022ed78d59841df
MatthiasChometon <matthias.chometon@emage-me.com>
2024-10-15 18:47:45 +0200

    feat: add translations for useMovieApiFetch

--- domain/film/composables/useMovieApiFetch.ts
+++ domain/film/composables/useMovieApiFetch.ts
@@ -3,5 +3,11 @@ type UseFetchOptions<T> = Parameters<typeof useFetch<T>>['1']
 type UseFetchReturn<T> = ReturnType<typeof useFetch<T>>
 
 export const useMovieApiFetch = <T>(request: UseFetchRequest<T>, options?: UseFetchOptions<T>): UseFetchReturn<T> => {
-	return useFetch<T>(request, { ...options, lazy: true, $fetch: useNuxtApp().$movieApiFetch })
+	const { locale } = useI18n()
+	return useFetch<T>(request, {
+		...options,
+		lazy: true,
+		$fetch: useNuxtApp().$movieApiFetch,
+		query: { language: locale, ...options?.query }
+	})
 }
