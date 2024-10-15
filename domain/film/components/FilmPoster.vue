50652a3b47699813d5e978679321e0b5103c02d3
MatthiasChometon <matthias.chometon@emage-me.com>
2024-10-15 18:45:36 +0200

    feat: add FilmPoster

---
+++ domain/film/components/FilmPoster.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{ film: Film }>()
+</script>
+
+<template>
+  <v-img
+    :aspect-ratio="2/3"
+    cover
+    eager
+    :src="film.posterUrl"
+  >
+    <template #placeholder>
+      <v-skeleton-loader
+        type="image"
+        height="100%"
+      />
+    </template>
+  </v-img>
+</template>
+
+<style>
+ .v-skeleton-loader__image {
+  height: 100%;
+ }
+</style>
