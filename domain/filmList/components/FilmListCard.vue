eab6df31894f086414d8058f3c1a7db291004e4b
MatthiasChometon <matthias.chometon@emage-me.com>
2024-10-15 18:48:47 +0200

    refactor: use FilmPoster in FilmListCard

--- domain/filmList/components/FilmListCard.vue
+++ domain/filmList/components/FilmListCard.vue
@@ -14,19 +14,7 @@ defineProps<{ film: Film }>()
           class="overflow-visible"
           :elevation="isHovering ? 24 : 0"
         >
-          <v-img
-            :aspect-ratio="2/3"
-            cover
-            eager
-            :src="film.posterUrl"
-          >
-            <template #placeholder>
-              <v-skeleton-loader
-                type="image"
-                height="100%"
-              />
-            </template>
-          </v-img>
+          <FilmPoster :film="film" />
         </v-card>
         <p :class="`my-2 text-body-1 ${isHovering ? 'text-white' : 'text-secondary'}`">
           {{ film.title }}
@@ -35,9 +23,3 @@ defineProps<{ film: Film }>()
     </v-hover>
   </NuxtLink>
 </template>
-
-<style>
- .v-skeleton-loader__image {
-  height: 100%;
- }
-</style>
