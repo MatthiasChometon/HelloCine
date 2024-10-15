3ffa355ac114952f8a9038685bc647ce707577b7
MatthiasChometon <matthias.chometon@emage-me.com>
2024-10-15 14:15:51 +0200

    feat: add film list page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,29 @@
+<script setup lang="ts">
+const { convertApiFilmToFilm } = useConvertApiFilmToFilm()
+const page = ref(1)
+const { t } = useI18n()
+const filmList = ref<Film[]>([])
+const { status } = await useMovieApiFetch<{ results: ApiFilm[] }>('/movie/now_playing', {
+  query: {
+    page
+  },
+  onResponse: ({ response }) => {
+    const filmListToAdd: Film[] = response._data.results.map((apiFilm: ApiFilm) => convertApiFilmToFilm(apiFilm))
+    filmList.value = [...filmList.value, ...filmListToAdd]
+  },
+  default: () => ({ results: [] })
+})
+
+const addNewPageFilms = () => {
+  page.value++
+}
+
+const titleTemplate = computed(() => `HelloCine - ${t('layout.bar.home')}`)
+useHead({
+  titleTemplate
+})
+</script>
+
 <template>
-  <div />
+  <FilmList :film-list="filmList" :status="status" @on-load-new-page="addNewPageFilms" />
 </template>
