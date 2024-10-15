d0998d8521a99fad7cf710fb6fe447744cbbc299
MatthiasChometon <matthias.chometon@emage-me.com>
2024-10-15 14:14:47 +0200

    feat: add movieApiUrl and movieApiToken to env

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,13 @@
+declare module '#app' {
+  interface NuxtApp {
+   $movieApiFetch: $Fetch<unknown, NitroFetchRequest>
+ }
+ }
+
 declare module '@nuxt/types/config/env' {
   interface NuxtOptionsEnv {
+    movieApiUrl: string
+    movieApiToken: string
   }
 }
 
